5bd722b7eca5988b268d9872b1129c458b9ac7cf
Saksham Shrey <sakshamshrey_work@icloud.com>
2024-04-06 11:37:51 +0530

    Initial Commit

---
+++ Hacker News/Hacker_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_NewsApp.swift
+//  Hacker News
+//
+//  Created by Saksham Shrey on 06/04/24.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
